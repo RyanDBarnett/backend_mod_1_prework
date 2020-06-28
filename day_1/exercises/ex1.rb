e24a6b1d7c6c63d1492a9c6686d6e55f13820b2c
RyanDBarnett <conner2468@gmail.com>
2020-06-28 10:51:48 -0600

    Complete ex1.rb

---
+++ day_1/exercises/ex1.rb
@@ -0,0 +1,10 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+puts "Look! I made my script print another line!"
+#puts "You won't see this one!"
